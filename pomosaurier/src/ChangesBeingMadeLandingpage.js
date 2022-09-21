5ff9ba895d6e3935914ac9d13d51672487d3955d
Nikita Wagner <nikita.leon.wagner@gmail.com>
2022-09-21 13:25:50 +0200

    FEATURE: add maintenance page
    addes page that shows that the page is being updated

---
+++ pomosaurier/src/ChangesBeingMadeLandingpage.js
@@ -0,0 +1,9 @@
+import "./ChangesBeingMadeLandingpageCSS.css";
+
+export default function ChangesBeingMadeLandingpage() {
+  return (
+    <div className="outsideContainer">
+      <p>Page is being updated...</p>
+    </div>
+  );
+}
