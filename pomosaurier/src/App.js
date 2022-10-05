1587908863a9c5a6573d821c08cff428457dee02
Nikita Wagner <nikita.leon.wagner@gmail.com>
2022-10-05 19:00:57 +0200

    FIX: chagned app js

--- pomosaurier/src/App.js
+++ pomosaurier/src/App.js
@@ -10,6 +10,7 @@ function App() {
         <Routes>
           <Route path="/changes" element={<ChangesBeingMadeLandingpage />} />
           <Route path="/pomosaurier" element={<Landingpage />} />
+          <Route path="/" element={<Landingpage />} />
         </Routes>
       </BrowserRouter>
     </>
